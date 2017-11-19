8b9a031ed06808d23a9c7ce3088becebfc82933f
seba <seba159>
2017-11-19 15:31:53 +0100

    klasa edytowana przez zgodny

--- src/Klasa1.java
+++ src/Klasa1.java
@@ -1,5 +1,6 @@
 public class Klasa1 {
     public void show() {
         System.out.println("show Klasa1");
+        System.out.println("klasa edytowana przez zgodny");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,5 @@ public class Main {
         System.out.println("Hello World!");
         Klasa1 klasa1=new Klasa1();
         Klasa2 klasa2= new Klasa2();
-
-        System.out.println("Zgodny");
     }
 }
