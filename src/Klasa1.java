5fddec37e8e569407bed2c2aabe4ca9bfbc0d824
seba <seba159>
2017-11-19 13:30:35 +0100

    metoda show 2

--- src/Klasa1.java
+++ src/Klasa1.java
@@ -2,4 +2,7 @@ public class Klasa1 {
     public void show() {
         System.out.println("show Klasa1 zmodyfikowala koza");
     }
+    public void show2() {
+        System.out.println("show2 Klasa1 zmodyfikowala koza");
+    }
 }
--- src/Klasa2.java
+++ src/Klasa2.java
@@ -3,4 +3,7 @@ public class Klasa2 {
     public void show() {
         System.out.println("show Klasa2 zmodyfikowala koza");
     }
+    public void show2() {
+        System.out.println("show2 Klasa2 zmodyfikowala koza");
+    }
 }
