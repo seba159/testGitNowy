494d03dbc92811ae360b143fdbcc4b3955e1a274
seba <seba159>
2017-11-19 13:32:36 +0100

    show2

--- src/Klasa1.java
+++ src/Klasa1.java
@@ -2,4 +2,7 @@ public class Klasa1 {
     public void show() {
         System.out.println("show Klasa1 zmodyfikowal slon");
     }
+    public void show2() {
+        System.out.println("show2 Klasa1 zmodyfikowal slon");
+    }
 }
