c072951b681e5baf822f7323206e066234d0d347
kkcm <koloczek@student.agh.edu.pl>
2017-11-20 00:16:17 +0100

    no message

--- src/Klasa2.java
+++ src/Klasa2.java
@@ -1,7 +1,7 @@
 public class Klasa2 {
 
     public void show() {
-        System.out.println("show Klasa2 zmodyfikowala koza2222");
+        System.out.println("show Klasa2 zmodyfikowala koza");
     }
     public void show2() {
         System.out.println("show2 Klasa2 zmodyfikowala koza");
